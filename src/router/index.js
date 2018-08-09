6e46b38b59a2f9ff2845a0654881c15edd5bb23d
PushyZheng <1437876073@qq.com>
2018-08-09 16:19:28 +0800

    8.9 reload

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,5 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-// 修改路由的懒加载：
-const Posts = () => import('@/components/Posts')
-const Catagory = () => import('@/components/Catagory')
-const Original = () => import('@/components/Original')
-const notfound = () => import('@/components/notfound')
-const AboutMe = () => import('@/components/AboutMe')
 
 Vue.use(Router)
 
@@ -23,7 +17,7 @@ export default new Router({
     {
       path: '/',
       name: 'Posts',
-      component: Posts
+      component: () => import('@/components/Posts')
     },
     {
       path: '/posts/',
@@ -32,22 +26,22 @@ export default new Router({
     {
       path: '/posts/catagory/:item',
       name:'Catagory',
-      component:Catagory
+      component:() => import('@/components/Catagory')
     },
     {
       path: '/posts/:post_id',
       name: 'Original',
-      component: Original
+      component: () => import('@/components/Original')
     },
     {
       path: '/aboutme',
       name: 'AboutMe',
-      component: AboutMe
+      component: () => import('@/components/AboutMe')
     },
     {
       path: '*',
       name: 'notfound',
-      component: notfound
+      component: () => import('@/components/notfound')
     },
   ],
   // 添加scrollBehavior方法，当切换路由时都跳转到页面的顶部 
